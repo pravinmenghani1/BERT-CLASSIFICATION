9de3e97c14b9cc0b7af612c0c376c180f0d0c1e4
Punit Jain <137204287+punit2006@users.noreply.github.com>
2025-06-25 22:21:02 +0530

    Create Tokenize_text.py

---
+++ Tokenize_text.py
@@ -0,0 +1,17 @@
+from load_model import tokenizer, model
+
+text = "Hello, Myself Punit Jain!"
+
+tokens = tokenizer.tokenize(text)
+print("\nTokens:", tokens)
+
+inputs = tokenizer(
+    text,
+    return_tensors="pt",       
+    truncation=True,           
+    padding="max_length",     
+    max_length=15              
+)
+print("\nEncoded Input (Dictionary):")
+print("Input IDs (Token IDs):", inputs["input_ids"])
+print("Attention Mask:", inputs["attention_mask"])
